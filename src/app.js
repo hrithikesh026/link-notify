917db9f55da43390d24d8f2a9d92d01ac5f6c2f2
Hrithikesh <hithikeshmylatty@gmail.com>
2021-06-15 18:39:07 +0530

    initial-commit

--- src/app.js
+++ src/app.js
@@ -1,94 +1,103 @@
 const path = require('path')
-const hbs  = require('hbs')
-const forecast = require('./utils/forecast')
-const geocode = require('./utils/geocode')
+// const hbs  = require('hbs')
+// const forecast = require('./utils/forecast')
+// const geocode = require('./utils/geocode')
 const express = require('express')
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
-const publicFilePath = path.join(__dirname,'../public') //To get absolute path to public folder
-const viewsPath = path.join(__dirname,'../templates/views') //default is views
-const partialsPath = path.join(__dirname,'../templates/partials')
+// const publicFilePath = path.join(__dirname,'../public') //To get absolute path to public folder
+// const viewsPath = path.join(__dirname,'../templates/views') //default is views
+// const partialsPath = path.join(__dirname,'../templates/partials')
 
 // console.log(publicFilePath)
 // console.log(partialsPath)
 
 
-app.set('view engine', 'hbs')
-app.set('views',viewsPath) //not required if views directory is used
-hbs.registerPartials(partialsPath)
-app.use(express.static(publicFilePath))
+// app.set('view engine', 'hbs')
+// app.set('views',viewsPath) //not required if views directory is used
+// hbs.registerPartials(partialsPath)
+// app.use(express.static(publicFilePath))
 
-app.get('',(req,res)=>{
-    res.render('index',{
-        title: 'Homepage',
-        name: 'Hrithikesh'
-    })
-})
+// app.get('',(req,res)=>{
+//     res.render('index',{
+//         title: 'Homepage',
+//         name: 'Hrithikesh'
+//     })
+// })
 
-app.get('/about',(req,res)=>{
+// app.get('/about',(req,res)=>{
 
-    res.render('about',{
-        title: 'About Page',
-        name: 'Hrithikesh'
-    })
-})
+//     res.render('about',{
+//         title: 'About Page',
+//         name: 'Hrithikesh'
+//     })
+// })
 
-app.get('/help',(req,res)=>{
-    res.render('help',{
-        title: 'Help Page',
-        name: 'Hrithikesh'
-    })
-})
+// app.get('/help',(req,res)=>{
+//     res.render('help',{
+//         title: 'Help Page',
+//         name: 'Hrithikesh'
+//     })
+// })
 
 
-app.get('/weather', (req,res)=>{
-    if(!req.query.address){
-        return res.send({
-            error: "Address Variable required"
-        })
-    }
-    const Address = req.query.address
-    geocode(Address, (error,{latitude,longitude,location} = {})=>{
-        if(error){
-            return res.send({error})
-        }
-        forecast(latitude,longitude,(error,forecastData)=>{
-            if(error){
-                return res.send({error})
-            }
-            res.send({
-                address: req.query.address,
-                location: location,
-                forecast: forecastData
-            })
-        })
-    })
+// app.get('/weather', (req,res)=>{
+//     if(!req.query.address){
+//         return res.send({
+//             error: "Address Variable required"
+//         })
+//     }
+//     const Address = req.query.address
+//     geocode(Address, (error,{latitude,longitude,location} = {})=>{
+//         if(error){
+//             return res.send({error})
+//         }
+//         forecast(latitude,longitude,(error,forecastData)=>{
+//             if(error){
+//                 return res.send({error})
+//             }
+//             res.send({
+//                 address: req.query.address,
+//                 location: location,
+//                 forecast: forecastData
+//             })
+//         })
+//     })
 
     
-})
+// })
 
-app.get('/help/*',(req,res)=>{
-    res.render('404notfound',{
-        name: 'Hrithikesh',
-        error: 404,
-        message: 'help article not found'
-    })
-})
+// app.get('/help/*',(req,res)=>{
+//     res.render('404notfound',{
+//         name: 'Hrithikesh',
+//         error: 404,
+//         message: 'help article not found'
+//     })
+// })
 
-app.get('*', (req,res)=>{
-    res.render('404notfound',{
-        name: 'Hrithikesh',
-        error: 404,
-        message: 'Page not found'
-    })
-})
+// app.get('*', (req,res)=>{
+//     res.render('404notfound',{
+//         name: 'Hrithikesh',
+//         error: 404,
+//         message: 'Page not found'
+//     })
+// })
 
 // app.com
 // app.com/help
 // app.com/pictures
+app.get('/', function (req, res) {
+    res.send('GET request to the homepage')
+  })
+  
+  // POST method route
+  app.post('/', function (req, res) {
+    res.send('POST request to the homepage')
+  })
+
 
 app.listen(port,()=>{
     console.log('server up and running on port '+ port)
---
+++ src/utils/cred.js
@@ -0,0 +1,5 @@
+const token = '1826845009:AAGaOn-NKh7z_XCWd5GL7RkGaRmJwcHEOPw';
+
+module.exports = {
+    token
+}
