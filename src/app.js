c0e535ff1674cdf00292eb78da25fb84ff94dbde
Hrithikesh <hithikeshmylatty@gmail.com>
2021-06-19 21:02:30 +0530

    Back to link-notify

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.get('/', function (req, res) {
   
   // POST method route
   app.post('/'+cred.token,async function (req, res) {
-    console.log(req)
+    
     
     res.send('{"status":"POST request successfull"}')
     
@@ -151,135 +151,6 @@ app.get('/', function (req, res) {
   }
   )
 
-  app.post('/'+cred.mahadevToken,async function (req, res) {
-
-    
-    res.send('{"status":"POST request successfull"}')
-    
-    if(req.body.message.text){
-      req.body.message.text = req.body.message.text.replace('@CEC_Form_FIller_bot','') //Filtering bot username
-      if(req.body.message.text =='/help'){
-        mybot.sendMessage(req.body.message.chat.id, instructions.help)
-      }
-
-      else if(req.body.message.text =='/listsubjects'){
-  
-        mybot.sendMessage(req.body.message.chat.id, instructions.listsubjects)
-      }
-
-      else if(req.body.message.text in subjects){ //if text is a subject command,
-        
-        await mongo.jobs.findOne({subject: req.body.message.text}, function (err, docs) {
-          if(err){
-              console.log('Something went wrong while fetching jobs')
-          }
-          else{
-              if(docs == null){ //If job doesn't already exists in job, create the job
-                  job = new mongo.jobs({
-                    subject: req.body.message.text,
-                    users: [req.body.message.chat.id.toString()]
-                  })
-                  job.save().then(()=>{
-                    console.log(job)
-                  }).catch((error)=>{
-                    console.log('Error! ',error)
-                  });
-                  mybot.sendMessage(req.body.message.chat.id, 'Successfull.\nWe will inform you when '+req.body.message.text.slice(1)+' form is enabled')
-              }
-              else{ //If job exits, append user to the job
-                users = docs.users
-                if(users.includes(req.body.message.chat.id)){
-                  mybot.sendMessage(req.body.message.chat.id, 'Reminder already exists for '+req.body.message.text.slice(1))
-                }
-                else{
-                  users.push(req.body.message.chat.id.toString())
-                  mongo.jobs.updateOne({subject: req.body.message.text}, {users: users}, (error,res)=>{
-                    if(error){
-                      console.log('error while updating table '+error)
-                    }
-                  })
-                  mybot.sendMessage(req.body.message.chat.id, 'Successfull.\nWe will inform you when '+req.body.message.text.slice(1)+' form is enabled')
-                }
-
-              }
-          }
-        });
-
-      }
-      else if(req.body.message.text =='/deleteactive'){ //for deleting  all the notification  of a user
-        await mongo.jobs.find({},async function (err, docs) {
-          if(err){
-            console.log('Error occured in delete'+err)
-          }else{
-            docs.forEach(async (doc)=>{
-              
-              var pos= -1
-              doc.users.forEach((item,ind)=>{
-                if(item == req.body.message.chat.id){
-                  pos = ind
-                }
-              })
-              if(pos!=-1){
-                doc.users.splice(pos,1)
-              }
-            
-              if(doc.users.length == 0){
-                await mongo.jobs.deleteOne({ subject: doc.subject });
-              }
-              else
-              await mongo.jobs.updateOne({subject: doc.subject},{users: doc.users},(err,doc)=>{
-                if(err){
-                  console.log('error while updating delete '+err)
-                }
-              })
-            })
-          }
-        });
-        mybot.sendMessage(req.body.message.chat.id, 'All notifications Deleted successfully!')
-      }
-      else if(req.body.message.text =='/showactive'){
-        await mongo.jobs.find({},async function (err, docs){
-          if(err){
-            console.log('Error occured in show active'+err)
-          }
-          else{
-            var sublist = []
-            docs.forEach(async (doc)=>{
-              if(doc.users.includes(req.body.message.chat.id.toString())){
-                sublist.push(doc.subject)
-              }
-            })
-            // console.log(sublist)
-            if(sublist.length == 0){
-              mybot.sendMessage(req.body.message.chat.id, instructions.noactive)
-            }
-            else{
-              var str = ''
-              // for(var item in sublist){
-              //   
-              // }
-              sublist.forEach((sub)=>{
-                str = str+'\n-'+sub.slice(1)
-              })
-              mybot.sendMessage(req.body.message.chat.id, 'Current active notifications are:'+str )
-            }
-          }
-        })
-      }
-      else{
-        console.log('Starting post request')
-        axios.post(cred.mahadevURI, req.body).then((response)=>{
-          console.log('Response from mahadev = '+response)
-        }).catch((error)=>{
-          console.log('Error from mahadev = '+error)
-
-        })
-      }
-
-      
-    }
-    
-  })
   
 
 app.listen(port,()=>{
--- src/utils/cred.js
+++ src/utils/cred.js
@@ -1,5 +1,4 @@
 const token = '1826845009:AAGaOn-NKh7z_XCWd5GL7RkGaRmJwcHEOPw';
-const mahadevToken  = '1850254148:AAGNTV2qPNvNot3oHwKrUVOfShqjws_Vc7g'
-const mahadevURI = 'https://grouphelp.top/bot/index.php?api=bot1850254148:AAGNTV2qPNvNot3oHwKrUVOfShqjws_Vc7g'
+
 password = 'e1aPsW3zpJOZ9NAS';
-module.exports ={ token, password,mahadevURI,mahadevToken}
+module.exports ={ token, password}
